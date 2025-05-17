af8a2afa17f8d47cddfbe420719c11361e6e99c2
MK-hobbyrepo <matt.kowalkowski@gmail.com>
2025-05-17 02:17:33 +0000

    updated bookbot

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from stats import get_word_count
+from stats import *
 
 def main():
-    get_word_count("books/frankenstein.txt")
-
+    import sys
+    if len(sys.argv) == 2:
+        book_path = sys.argv[1]
+        list_char_counts = gen_num_char_list(book_path)
+        report(list_char_counts=list_char_counts, filepath=book_path)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
--- stats.py
+++ stats.py
@@ -1,8 +1,44 @@
-def get_word_count(filepath):
+def get_num_words(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     list_words = file_contents.split()
     num_words = len(list_words)
-    message = f"{num_words} words found in the document"
+    message = f"Found {num_words} total words"
     #print(list_words)
     print(message)
+
+def get_num_chars(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    #file contents is a string
+    char_dict = {}
+    chars = list(file_contents)
+    for char in chars:
+        char = char.lower()
+        if char in char_dict.keys():
+            #key exists
+            char_dict[char] += 1
+        else:
+            #init key
+            char_dict[char] = 1
+    return char_dict
+
+def gen_num_char_list(filepath):
+    num_char_dict = get_num_chars(filepath)
+    #print("num_char_dict: " + str(num_char_dict))
+    data = [{"char":key, "num":value} for key,value in num_char_dict.items()]
+    #print("data:" + str(data))
+    data.sort(key=lambda d: d["num"],reverse=True)
+    return data
+
+def report(list_char_counts, filepath):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + filepath + "...")
+    print("----------- Word Count ----------")
+    get_num_words(filepath=filepath)
+    print("--------- Character Count -------")
+    for entry in list_char_counts:
+        if entry["char"].isalpha():
+            print(str(entry["char"]) + ": "+ str(entry["num"]))
+    print("============= END ===============")
+    
